b5d7e4901cab95e7e5c96eccbc96667aba0bc561
Marc Granier <mgranier@comcast.net>
2022-12-15 17:15:14 -0500

    Implementing JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const bcrypt = require('bcryptjs');
 
 const User = require('../../models/User');
@@ -61,10 +63,24 @@ router.post(
 
 			//Return jsonwebtoken
 
-			res.send('User registered');
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: '5 days' },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
 		} catch (err) {
 			console.error(err.message);
-			res.status(500).send('Server Error');
+			res.status(500).send('Server error');
 		}
 	}
 );
